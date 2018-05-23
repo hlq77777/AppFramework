b3adfaf41a49f8a98a35838107d3661e62500a6c
KingWong <hlq777@msn.com>
2018-05-23 17:29:09 +0800

    updata file

---
+++ app/src/main/java/com/android/kingwong/kingwongproject/bean/UserInfo.java
@@ -0,0 +1,17 @@
+package com.android.kingwong.kingwongproject.bean;
+
+import java.io.Serializable;
+
+public class UserInfo implements Serializable {
+    public int userID ;
+    public String mobile ;
+    public String token ;
+    public String name ;
+    public String avatar ;// 头像url
+    public String jid ;
+    public int deviceType ;
+    public String createTime ;
+    public boolean isLogin;
+    public String popActivityTitle;
+}
+
---
+++ appframework/src/main/java/com/android/kingwong/appframework/ehttp/callback/BitmapCallback.java
@@ -0,0 +1,20 @@
+package com.android.kingwong.appframework.ehttp.callback;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import okhttp3.ResponseBody;
+
+/**
+ *
+ * Created by baozhong 2016/02/01
+ */
+public abstract class BitmapCallback extends HttpCallback<Bitmap>
+{
+    @Override
+    public Bitmap parseResponse(ResponseBody body) throws Exception
+    {
+        return BitmapFactory.decodeStream(body.byteStream());
+    }
+
+}
